2839e7ea1bf9f9370a0b477c8a8833ccf9338815
Erwin Rossen <erwin.rossen@fashion.cloud>
2022-06-07 21:22:54 +0200

    Setup for multiple zones

--- src/game.py
+++ src/game.py
@@ -13,7 +13,15 @@ from utils import DATA_DIR
 
 
 class Game:
-    current_game_file = os.path.join(DATA_DIR, 'current', constants.GAME_DATA_FILE)
+    _CURRENT_GAME_FILE = os.path.join(DATA_DIR, 'current', constants.GAME_DATA_FILE)
+
+    _keep_looping: bool
+    _zone: str
+    _obstacles: List[Obstacle]
+    _walkables: List[Walkable]
+    _player: Player
+    _monsters: List[Monster]
+    _all_sprites: Optional[pygame.sprite.Group]
 
     def __init__(self):
         self._init_pygame()
@@ -34,48 +42,47 @@ class Game:
     def all_sprites(self) -> pygame.sprite.Group:
         if not self._all_sprites:
             self._all_sprites = pygame.sprite.Group()
-            for elem in self.walkables:
+            for elem in self._walkables:
                 self._all_sprites.add(elem)
-            for elem in self.obstacles:
+            for elem in self._obstacles:
                 self._all_sprites.add(elem)
-            for monster in self.monsters:
+            for monster in self._monsters:
                 self._all_sprites.add(monster)
-            self._all_sprites.add(self.player)
+            self._all_sprites.add(self._player)
         return self._all_sprites
 
-    def _load_tiles(self) -> Tuple[List[Obstacle], List[Walkable]]:
-        obstacles = []
-        walkables = []
-        filepath = os.path.join(DATA_DIR, constants.MAPFILE)
+    def _load_tiles(self):
+        self._obstacles = []
+        self._walkables = []
+        filepath = os.path.join(DATA_DIR, 'zones', self._zone, constants.MAPFILE)
         with open(filepath, 'r') as f:
             world_map = [i.strip() for i in f.readlines()]
         for col, tiles in enumerate(world_map):
             for row, tile in enumerate(tiles):
                 if tile == '1':
-                    obstacles.append(Obstacle(x=row, y=col))
+                    self._obstacles.append(Obstacle(x=row, y=col))
                 elif tile == '.':
-                    walkables.append(Walkable(x=row, y=col))
+                    self._walkables.append(Walkable(x=row, y=col))
                 elif tile == 'P':
-                    walkables.append(Walkable(x=row, y=col))
+                    self._walkables.append(Walkable(x=row, y=col))
                 elif tile == 'M':
-                    walkables.append(Walkable(x=row, y=col))
+                    self._walkables.append(Walkable(x=row, y=col))
 
-        return obstacles, walkables
-
-    def _load_game_data(self) -> Tuple[Player, List[Monster]]:
-        if not os.path.isfile(self.current_game_file):
+    def _load_game_data(self):
+        if not os.path.isfile(self._CURRENT_GAME_FILE):
             src_filepath = os.path.join(DATA_DIR, 'original', constants.GAME_DATA_FILE)
-            copyfile(src_filepath, self.current_game_file)
-        with open(self.current_game_file, 'r') as f:
+            copyfile(src_filepath, self._CURRENT_GAME_FILE)
+        with open(self._CURRENT_GAME_FILE, 'r') as f:
             game_data = json.load(f)
-        player = Player.from_json(game_data['player'])
-        monsters = [Monster.from_json(monster_data) for monster_data in game_data['monsters']]
-        return player, monsters
+        self._zone = game_data['zone']
+        self._player = Player.from_json(game_data['player'])
+        self._monsters = [Monster.from_json(monster_data) for monster_data in game_data['monsters']]
 
     def _save_game_data(self):
-        game_data = {'player': self.player.as_json(),
-                     'monsters': [monster.as_json() for monster in self.monsters]}
-        with open(self.current_game_file, 'w') as f:
+        game_data = {'zone': self._zone,
+                     'player': self._player.as_json(),
+                     'monsters': [monster.as_json() for monster in self._monsters]}
+        with open(self._CURRENT_GAME_FILE, 'w') as f:
             json.dump(game_data, f, indent=2)
 
     def handle_events(self):
@@ -90,18 +97,18 @@ class Game:
                 if event.key == pygame.K_r:
                     # TODO: This should not be so easy, but good for debugging
                     print('Restart')
-                    os.remove(self.current_game_file)
+                    os.remove(self._CURRENT_GAME_FILE)
                     self.restart_game()
                 if event.key == pygame.K_LEFT:
-                    self.player.move(direction=constants.LEFT, obstacles=self.obstacles)
+                    self._player.move(direction=constants.LEFT, obstacles=self._obstacles)
                 elif event.key == pygame.K_RIGHT:
-                    self.player.move(direction=constants.RIGHT, obstacles=self.obstacles)
+                    self._player.move(direction=constants.RIGHT, obstacles=self._obstacles)
                 elif event.key == pygame.K_DOWN:
-                    self.player.move(direction=constants.DOWN, obstacles=self.obstacles)
+                    self._player.move(direction=constants.DOWN, obstacles=self._obstacles)
                 elif event.key == pygame.K_UP:
-                    self.player.move(direction=constants.UP, obstacles=self.obstacles)
+                    self._player.move(direction=constants.UP, obstacles=self._obstacles)
                 elif event.key == pygame.K_h:
-                    monster = self.monster_on_tile(self.player.x, self.player.y)
+                    monster = self.monster_on_tile(self._player.x, self._player.y)
                     if not monster:
                         print('Player swings at the air')
                         return False
@@ -110,7 +117,7 @@ class Game:
                         return False
                     print("Fight!!!")
                     self.dialog_have_a_fight(monster)
-                    if self.player.is_dead():
+                    if self._player.is_dead():
                         self.player_died()
                 self._save_game_data()
 
@@ -127,10 +134,10 @@ class Game:
         Return the monster on the given tile, or None if there is no monster on the tile
         """
 
-        return next((monster for monster in self.monsters if monster.x == x and monster.y == y), None)
+        return next((monster for monster in self._monsters if monster.x == x and monster.y == y), None)
 
     def dialog_have_a_fight(self, monster):
-        fight_dialog = DialogFight(self.player, monster)
+        fight_dialog = DialogFight(self._player, monster)
         fight_dialog.main()
         self._save_game_data()
         self.restart_game()
@@ -138,13 +145,13 @@ class Game:
     def restart_game(self):
         self._init_pygame()
         self._all_sprites = None
-        self.keep_looping = True
-        self.obstacles, self.walkables = self._load_tiles()
-        self.player, self.monsters = self._load_game_data()
+        self._keep_looping = True
+        self._load_game_data()
+        self._load_tiles()
 
     def player_died(self):
         s = 'You are dead! Game over.'
         mydialog = TextDialog(s)
         mydialog.main()
-        self.keep_looping = False
+        self._keep_looping = False
         self._init_pygame()
